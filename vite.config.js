8f674159cd945ca3c2f4c44284f32d50c53a4fe4
tammiehurdle3 <tammiehurdle3@gmail.com>
2025-09-10 02:31:42 +0100

    Feat: Add and configure sitemap generator

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import sitemap from 'vite-plugin-sitemap'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    sitemap({
+    sitemap({ 
       hostname: 'https://ontracourier.us',
-      exclude: ['/tracking'] // <-- Add this line
-    })
+      exclude: ['/tracking'] // This excludes the tracking page
+    }) 
   ],
 })
