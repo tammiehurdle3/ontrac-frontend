653f90b1877d9b9c5424a634d0f8a3caa0f9b44b
tammiehurdle3 <tammiehurdle3@gmail.com>
2025-09-17 02:04:28 +0100

    remove unwanted newsletter

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,6 @@
 // src/pages/HomePage.jsx
 import React from 'react';
 import TrackingForm from '../components/TrackingForm';
-import SubscriptionForm from '../components/SubscriptionForm';
 
 function HomePage() {
   return (
@@ -115,11 +114,6 @@ function HomePage() {
               </a>
             </div>
           </div>
-        </div>
-      </section>
-      <section className="newsletter-signup" style={{ backgroundColor: '#f7f7f7' }}>
-        <div className="container">
-          <SubscriptionForm />
         </div>
       </section>
     </main>
